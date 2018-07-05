92e6ef3806f6bed9f49b9907d97e3a32df93bbfb
Vinayak Sinha <vinayaksinha@zoho.com>
2018-07-05 21:52:50 +0530

    basic template ready

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Device Portal :)
+      </div>
+    );
+  }
+}
+
+export default App;
